5773f23c25ad5d0f6a68eb3407a40b13cc474715
sumit11211 <sumitprajapatfirst@tvsmotor.com>
2024-03-14 13:07:00 +0530

    utilised code (with less condition statements)

--- LoginDemoProject/View/RegistrationViewController.swift
+++ LoginDemoProject/View/RegistrationViewController.swift
@@ -36,57 +36,52 @@ class RegistrationViewController: UIViewController {
               let lastName = lastNameTextField.text, !lastName.isEmpty,
               let email = emailTextField.text, !email.isEmpty,
               let password = passwordTextField.text, !password.isEmpty else {
-            // If any of the required fields are empty, show an alert
-            let alertController = UIAlertController(title: "Error", message: Constants.detailsNotFilledWarning, preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            present(alertController, animated: true, completion: nil)
+            showAlert(title: "Error", message: Constants.detailsNotFilledWarning)
             return
         }
         
-        if isValidEmail(email) {
-            let realm = try! Realm()
-            let existingUser = realm.objects(Registration.self).filter("email = %@", email).first
-            if existingUser != nil {
-                // User already registered, show an alert
-                let alertController = UIAlertController(title: "Email Already Registered", message: nil, preferredStyle: .alert)
-                alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                present(alertController, animated: true, completion: nil)
-                return
-            }
-            
-            if isValidPassword(password) {
-                // Password meets the required conditions, proceed with registration
-                let realm = try! Realm()
-                // Create a new Registration object and set its properties
-                let registration = Registration()
-                registration.firstName = firstName
-                registration.lastName = lastName
-                registration.email = email
-                registration.password = password
-                // Save the registration object to the Realm database
-                try! realm.write {
-                    realm.add(registration)
-                }
-                
-                let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                if let descriptionViewController = storyboard.instantiateViewController(withIdentifier: "DescriptionViewController") as? DescriptionViewController {
-                    descriptionViewController.enteredEmail = email
-                    descriptionViewController.entryInformation = Constants.registrationStatus
-                    navigationController?.pushViewController(descriptionViewController, animated: true)
-                }
-            } else {
-                let alertController = UIAlertController(title: "Password Format Error", message: Constants.passwordFormatWarning, preferredStyle: .alert)
-                alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                present(alertController, animated: true, completion: nil)
-                
-            }
-        } else {
-            // Invalid email format, show an alert
-            let alertController = UIAlertController(title: "Error", message: Constants.emailFormatWarning, preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-            present(alertController, animated: true, completion: nil)
+        guard isValidEmail(email) else {
+            showAlert(title: "Error", message: Constants.emailFormatWarning)
+            return
+        }
+        
+        guard isValidPassword(password) else {
+            showAlert(title: "Password Format Error", message: Constants.passwordFormatWarning)
+            return
+        }
+        
+        let realm = try! Realm()
+        guard realm.objects(Registration.self).filter("email = %@", email).first == nil else {
+            showAlert(title: "Email Already Registered", message: nil)
+            return
+        }
+        
+        // Create a new Registration object and set its properties
+        let registration = Registration()
+        registration.firstName = firstName
+        registration.lastName = lastName
+        registration.email = email
+        registration.password = password
+        
+        // Save the registration object to the Realm database
+        try! realm.write {
+            realm.add(registration)
+        }
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let descriptionViewController = storyboard.instantiateViewController(withIdentifier: "DescriptionViewController") as? DescriptionViewController {
+            descriptionViewController.enteredEmail = email
+            descriptionViewController.entryInformation = Constants.registrationStatus
+            navigationController?.pushViewController(descriptionViewController, animated: true)
         }
     }
+
+    func showAlert(title: String, message: String?) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+
     // Method to verify Email format
     func isValidEmail(_ email: String) -> Bool {
         let emailRegex = Constants.emailFormatConditions
